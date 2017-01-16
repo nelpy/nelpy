a1c27527a516507cf65c36c19d03d6a51a670333
Etienne <era3@rice.edu>
2017-01-15 19:04:02 -0600

    getting ready for release

--- nelpy/__init__.py
+++ nelpy/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 nelpy
 =====
@@ -19,41 +18,11 @@ from .objects import (EventArray,
                       BinnedSpikeTrainArray)
 
 # TODO: decide on which utils to expose:
-from .utils import (find_nearest_idx,
-                    find_nearest_indices)
+# from .utils import (find_nearest_idx,
+#                     find_nearest_indices)
 
 # from .hmmutils import PoissonHMM
 
 # from .plotting import plot
 
-__version__ = '0.0.6'
-=======
-"""
-nelpy
-=====
-
-``nelpy`` is a neuroelectrophysiology object model and data analysis suite
-based on the python-vdmlab project (https://github.com/mvdm/vandermeerlab),
-and inspired by the neuralensemble.org NEO project
-(see http://neo.readthedocs.io/en/0.4.0/core.html).
-"""
-
-from .objects import (EventArray,
-                      EpochArray,
-                      AnalogSignal,
-                      AnalogSignalArray,
-                      SpikeTrain,
-                      SpikeTrainArray,
-                      BinnedSpikeTrain,
-                      BinnedSpikeTrainArray)
-
-# TODO: decide on which utils to expose:
-from .utils import (find_nearest_idx,
-                    find_nearest_indices)
-
-# from .hmmutils import PoissonHMM
-
-# from .plotting import plot
-
-__version__ = '0.0.6'
->>>>>>> feature/plotting
+__version__ = '0.0.8'
--- nelpy/plotting/__init__.py
+++ nelpy/plotting/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 nelpy.plotting
 =====
@@ -24,31 +23,4 @@ from .core import (plot,
 
 from .scalebar import add_scalebar
 
-=======
-"""
-nelpy.plotting
-=====
-
-This is the nelpy plotting module.
-"""
-
-# from ..objects import (EventArray,
-#                        EpochArray,
-#                        AnalogSignal,
-#                        AnalogSignalArray,
-#                        SpikeTrain,
-#                        SpikeTrainArray,
-#                        BinnedSpikeTrain,
-#                        BinnedSpikeTrainArray)
-
-from .core import (plot,
-                   comboplot,
-                   raster,
-                   matshow,
-                   overviewstrip,
-                   epoch_plot)
-
-from .scalebar import add_scalebar
-
->>>>>>> feature/plotting
 __version__ = '0.0.2'  # should I maintain a separate version for this?
