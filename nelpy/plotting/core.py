df643843caaab11f8f8035864bd7cc1066c7fed5
eackermann <era@rice.edu>
2017-01-19 11:35:35 -0600

    fixes for raster plot

--- nelpy/plotting/core.py
+++ nelpy/plotting/core.py
@@ -143,187 +143,69 @@ def overviewstrip():
     """
     raise NotImplementedError("overviewstripplot() not implemented yet")
 
-def raster(data, *, cmap=None, color=None, legend=True, ax=None, plot_support=True, **kwargs):
-    """Plot one or more timeseries with flexible representation of uncertainty.
-
-    This function is intended to be used with data where observations are
-    nested within sampling units that were measured at multiple timepoints.
-
-    It can take data specified either as a long-form (tidy) DataFrame or as an
-    ndarray with dimensions (unit, time) The interpretation of some of the
-    other parameters changes depending on the type of object passed as data.
-
-    Parameters
-    ----------
-    data : DataFrame or ndarray
-        Data for the plot. Should either be a "long form" dataframe or an
-        array with dimensions (unit, time, condition). In both cases, the
-        condition field/dimension is optional. The type of this argument
-        determines the interpretation of the next few parameters. When
-        using a DataFrame, the index has to be sequential.
-    time : string or series-like
-        Either the name of the field corresponding to time in the data
-        DataFrame or x values for a plot when data is an array. If a Series,
-        the name will be used to label the x axis.
-    unit : string
-        Field in the data DataFrame identifying the sampling unit (e.g.
-        subject, neuron, etc.). The error representation will collapse over
-        units at each time/condition observation. This has no role when data
-        is an array.
-    value : string
-        Either the name of the field corresponding to the data values in
-        the data DataFrame (i.e. the y coordinate) or a string that forms
-        the y axis label when data is an array.
-    condition : string or Series-like
-        Either the name of the field identifying the condition an observation
-        falls under in the data DataFrame, or a sequence of names with a length
-        equal to the size of the third dimension of data. There will be a
-        separate trace plotted for each condition. If condition is a Series
-        with a name attribute, the name will form the title for the plot
-        legend (unless legend is set to False).
-    err_style : string or list of strings or None
-        Names of ways to plot uncertainty across units from set of
-        {ci_band, ci_bars, boot_traces, boot_kde, unit_traces, unit_points}.
-        Can use one or more than one method.
-    ci : float or list of floats in [0, 100]
-        Confidence interval size(s). If a list, it will stack the error
-        plots for each confidence interval. Only relevant for error styles
-        with "ci" in the name.
-    interpolate : boolean
-        Whether to do a linear interpolation between each timepoint when
-        plotting. The value of this parameter also determines the marker
-        used for the main plot traces, unless marker is specified as a keyword
-        argument.
-    color : seaborn palette or matplotlib color name or dictionary
-        Palette or color for the main plots and error representation (unless
-        plotting by unit, which can be separately controlled with err_palette).
-        If a dictionary, should map condition name to color spec.
-    estimator : callable
-        Function to determine central tendency and to pass to bootstrap
-        must take an ``axis`` argument.
-    n_boot : int
-        Number of bootstrap iterations.
-    err_palette : seaborn palette
-        Palette name or list of colors used when plotting data for each unit.
-    err_kws : dict, optional
-        Keyword argument dictionary passed through to matplotlib function
-        generating the error plot,
-    legend : bool, optional
-        If ``True`` and there is a ``condition`` variable, add a legend to
-        the plot.
-    ax : axis object, optional
-        Plot in given axis; if None creates a new figure
-    kwargs :
-        Other keyword arguments are passed to main plot() call
-
-    Returns
-    -------
-    ax : matplotlib axis
-        axis with plot data
-
-    Examples
-    --------
-
-    Plot a trace with translucent confidence bands:
-
-    .. plot::
-        :context: close-figs
-
-        >>> import numpy as np; np.random.seed(22)
-        >>> import seaborn as sns; sns.set(color_codes=True)
-        >>> x = np.linspace(0, 15, 31)
-        >>> data = np.sin(x) + np.random.rand(10, 31) + np.random.randn(10, 1)
-        >>> ax = sns.tsplot(data=data)
-
-    Plot a long-form dataframe with several conditions:
-
-    .. plot::
-        :context: close-figs
-
-        >>> gammas = sns.load_dataset("gammas")
-        >>> ax = sns.tsplot(time="timepoint", value="BOLD signal",
-        ...                 unit="subject", condition="ROI",
-        ...                 data=gammas)
-
-    Use error bars at the positions of the observations:
-
-    .. plot::
-        :context: close-figs
-
-        >>> ax = sns.tsplot(data=data, err_style="ci_bars", color="g")
-
-    Don't interpolate between the observations:
-
-    .. plot::
-        :context: close-figs
-
-        >>> import matplotlib.pyplot as plt
-        >>> ax = sns.tsplot(data=data, err_style="ci_bars", interpolate=False)
-
-    Show multiple confidence bands:
-
-    .. plot::
-        :context: close-figs
-
-        >>> ax = sns.tsplot(data=data, ci=[68, 95], color="m")
-
-    Use a different estimator:
-
-    .. plot::
-        :context: close-figs
-
-        >>> ax = sns.tsplot(data=data, estimator=np.median)
-
-    Show each bootstrap resample:
-
-    .. plot::
-        :context: close-figs
-
-        >>> ax = sns.tsplot(data=data, err_style="boot_traces", n_boot=500)
-
-    Show the trace from each sampling unit:
-
-
-    .. plot::
-        :context: close-figs
-
-        >>> ax = sns.tsplot(data=data, err_style="unit_traces")
-
-    """
-    # TODO: change y-axis formatter to be only integers, and add default
-    # labels
-
+def raster(data, *, cmap=None, color=None, legend=True, ax=None, plot_support=True, lw=None, lh=None, reindex = None,**kwargs):
+    """Docstring goes here."""
+    
     # Sort out default values for the parameters
     if ax is None:
         ax = plt.gca()
     if cmap is None and color is None:
-        color = 'k'
-
+        color = '0.25'
+    if lw is None:
+        lh = 1.5
+    if lh is None:
+        lh = 0.95
+    if reindex is None:
+        reindex = False
+    
+    hh = lh/2.0  # half the line height
+    
     # Handle different types of input data
     if isinstance(data, SpikeTrain):
         print("plotting SpikeTrain")
         raise NotImplementedError(
             "plotting {} not yet supported".format(str(type(data))))
     elif isinstance(data, SpikeTrainArray):
-        yrange = [.5, data.n_units + .5]
+        # the following example code can be used (modified) to correctly label
+        # only those units which have spike trains on the axis (cumulative)
+        #######################################################################
+#         ylabels = [item.get_text() for item in ax.get_yticklabels()]
+#         ylabels[1] = 'Testing'
+#         ax.set_yticklabels(ylabels)
+        #######################################################################
+        
+        prev_yrange = ax.get_ylim()
+        # hacky fix for default empty axes:
+        if prev_yrange[0] == 0:
+            prev_yrange = [np.infty, 1]
+        
+        if reindex:
+            minunit = 1
+            maxunit = data.n_units
+            unitlist = range(1, data.n_units + 1)
+        else:
+            minunit = np.array(data.unit_ids).min()
+            maxunit = np.array(data.unit_ids).max()
+            unitlist = data.unit_ids
+        
+        minunit = int(np.min([np.ceil(prev_yrange[0]), minunit]))
+        maxunit = int(np.max([np.floor(prev_yrange[1]), maxunit]))
 
+        yrange = [minunit - 0.5, maxunit + 0.5]
+       
         if cmap is not None:
             colors = cmap(np.linspace(0.25, 0.75, data.n_units)) # TODO: if we go from 0 then most colormaps are invisible at one end of the spectrum
-            for unit, spiketrain in enumerate(data.time):
-                ax.vlines(spiketrain, unit + .55, unit + 1.45, colors=colors[unit], **kwargs)
+            for unit, spiketrain in zip(unitlist, data.time):
+                ax.vlines(spiketrain, unit - hh, unit + hh, colors=colors[unit], lw=lw, **kwargs)
         else:  # use a constant color:
-            for unit, spiketrain in enumerate(data.time):
-                ax.vlines(spiketrain, unit + .55, unit + 1.45, colors=color, **kwargs)
+            for unit, spiketrain in zip(unitlist, data.time):
+                ax.vlines(spiketrain, unit - hh, unit + hh, colors=color, lw=lw, **kwargs)
 
         # change y-axis labels to integers
-        yint = range(1, data.n_units+1)
+        yint = range(minunit, maxunit + 1)
         ax.set_yticks(yint)
-
+        
         ax.set_ylim(yrange)
-
-        # # plot the support epochs:
-        # if plot_support:
-        #     epoch_plot(ax=ax, epochs=data.support, height=data.n_units+1, fc='0.5', ec=None, alpha=0.2)
 
     elif isinstance(data, EpochArray):
         # TODO: how can I figure out an appropriate height when plotting a spiketrain support?
@@ -331,23 +213,6 @@ def raster(data, *, cmap=None, color=None, legend=True, ax=None, plot_support=Tr
     else:
         raise NotImplementedError(
             "plotting {} not yet supported".format(str(type(data))))
-
-    # ax.plot(x, central_data, color=color, label=label, **kwargs)
-
-    # # Pad the sides of the plot only when not interpolating
-    # ax.set_xlim(x.min(), x.max())
-    # x_diff = x[1] - x[0]
-    # if not interpolate:
-    #     ax.set_xlim(x.min() - x_diff, x.max() + x_diff)
-
-    # # Add the plot labels
-    # if xlabel is not None:
-    #     ax.set_xlabel(xlabel)
-    # if ylabel is not None:
-    #     ax.set_ylabel(ylabel)
-    # if legend:
-    #     ax.legend(loc=0, title=legend_name)
-
     return ax
 
 def epoch_plot(ax, epochs, height=None, fc='0.5', ec=None,
