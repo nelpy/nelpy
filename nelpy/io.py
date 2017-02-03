1c0766cab2ad33386d24add4a1ad95ad2cfc348a
eackermann <era@rice.edu>
2017-02-03 13:49:32 -0600

    added hc3 load example to io.py

--- nelpy/io.py
+++ nelpy/io.py
@@ -3,6 +3,54 @@
 
 This entire module will probably be deprecated soon, so don't rely
 on any ot this to keep working!
+
+Example
+=======
+
+datadirs = ['/home/etienne/Dropbox/neoReader/Data',
+            'C:/etienne/Dropbox/neoReader/Data',
+            'C:/Users/etien/Dropbox/neoReader/Data',
+            '/Users/etienne/Dropbox/neoReader/Data',
+            'D:/Dropbox/neoReader/Data']
+
+fileroot = next( (dir for dir in datadirs if os.path.isdir(dir)), None)
+
+if fileroot is None:
+    raise FileNotFoundError('datadir not found')
+
+exp_data = dict()
+myhmm = dict()
+data = dict()
+sessiondate = dict()
+
+sessions = ['session1', 'session2']
+
+animal = 'gor01'; month,day = (6,7); sessiondate['session1'] = '11-26-53'; sessiondate['session2'] = '16-40-19' # 91 units, but session one has missing position data
+# animal = 'gor01'; month,day = (6,12); sessiondate['session1'] = '15-55-31'; sessiondate['session2'] = '16-53-46' # 55 units
+# animal = 'gor01'; month,day = (6,13); sessiondate['session1'] = '14-42-6'; sessiondate['session2'] = '15-22-3'
+
+for session in sessions:
+
+    exp_data[session] = dict()
+
+    exp_kws = dict(fileroot = fileroot,
+               animal = animal,
+               session = sessiondate[session],
+               month = month,
+               day = day,
+               includeUnsortedSpikes=False, # should be True for MUA analysis!
+               verbose = False)
+
+    exp_data[session]['spikes'] = nel.load_hc3_data(datatype='spikes', fs=32552, **exp_kws)
+#     exp_data[session]['eeg'] = load_data(datatype='eeg', channels=[0,1,2], fs=1252, starttime=0, **exp_kws)
+#     exp_data[session]['posdf'] = load_data(datatype='pos',**exp_kws)
+#     exp_data[session]['speed'] = klab.get_smooth_speed(exp_data[session]['posdf'],fs=60,th=8,cutoff=0.5,showfig=False,verbose=False)
+
+# make st1 and st2 explicitly available:
+st1 = exp_data['session1']['spikes']
+st2 = exp_data['session2']['spikes']
+
+
 """
 
 __all__ = ['load_hc3_data']
@@ -35,13 +83,13 @@ def load_hc3_data(fileroot, animal='gor01', year=2006, month=6, day=7, session='
     fileroot = os.path.normpath(fileroot)
     anim_prefix = "{}-{}-{}".format(animal,month,day)
     session_prefix = "{}-{}-{}_{}".format(year,month,day,session)
-    sessiondir = "{}/{}/{}".format(fileroot, anim_prefix, session_prefix) 
+    sessiondir = "{}/{}/{}".format(fileroot, anim_prefix, session_prefix)
 
     if (datatype=='spikes'):
         # NOTE: st_array[0] always corresponds to unsortable spikes (not mechanical noise). However, when includeUnsortedSpikes==True, then it gets populated
         #       with spike times; else, it just remains an empty list []
 
-        #filename = "{}/{}/{}/{}.clu.1".format(fileroot, anim_prefix, session_prefix, session_prefix) 
+        #filename = "{}/{}/{}/{}.clu.1".format(fileroot, anim_prefix, session_prefix, session_prefix)
         filename = "{}/{}/{}/{}".format(fileroot, anim_prefix, session_prefix, session_prefix)
         #print(filename)
         if verbose:
