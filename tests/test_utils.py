88d7db9f256e0432d44996a5eeada152ca921ce7
eackermann <era@rice.edu>
2017-03-07 15:02:13 -0600

    added some tests for nelpy.utils

---
+++ tests/test_utils.py
@@ -0,0 +1,23 @@
+from nelpy.utils import *
+
+class TestUtils:
+
+    def test_linear_merge1(self):
+        """Merge two sorted lists"""
+        merged = linear_merge([1,2,4],[3,5,6])
+        assert list(merged) == [1,2,3,4,5,6]
+
+    def test_linear_merge2(self):
+        """Merge non-empty and empty lists"""
+        merged = linear_merge([1,2,4],[])
+        assert list(merged) == [1,2,4]
+
+    def test_linear_merge3(self):
+        """Merge empty and non-empty lists"""
+        merged = linear_merge([],[3,5,6])
+        assert list(merged) == [3,5,6]
+
+    def test_linear_merge4(self):
+        """Merge two unsorted lists"""
+        merged = linear_merge([1,4,2],[3,6,5])
+        assert list(merged) == [1,3,4,2,6,5]
