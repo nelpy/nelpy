7d1d3c7619a5616c97cf8b1cc796f08f1c38985c
Etienne Ackermann <Etienne Ackermann>
2017-03-26 21:59:03 -0500

    ENH PrettyInt
    Now PrettyInt works on __repr__ as well as on print with __str__

--- nelpy/utils.py
+++ nelpy/utils.py
@@ -182,6 +182,9 @@ class PrettyInt(int):
     def __init__(self, val):
         self.val = val
 
+    def __str__(self):
+        return '{:,}'.format(self.val)
+
     def __repr__(self):
         return '{:,}'.format(self.val)
 
