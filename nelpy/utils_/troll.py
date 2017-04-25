2c753df119721bc9ae90fd821fc65f9e19762000
Etienne Ackermann <Etienne Ackermann>
2017-04-25 01:32:52 -0500

    no commit message

--- nelpy/utils_/troll.py
+++ nelpy/utils_/troll.py
@@ -1,8 +1,21 @@
 import getpass
-from .. import core
-from .decorators import *
+import inspect
 
-@add_prop_to_class(core.AnalogSignalArray)
+from .. import core
+# from .decorators import *
+
+def add_prop_to_class(cls, name):
+    """working"""
+    if not inspect.isclass(cls):
+        raise TypeError("class expected!")
+    def decorator(f):
+        if not hasattr(cls, '__perinstance'):
+            cls.__perinstance = True
+        setattr(cls, name, property(f))
+        return f
+    return decorator
+
+@add_prop_to_class(core.AnalogSignalArray, getpass.getuser())
 def whos_your_daddy(self):
-    return getpass.getuser()
+    return getpass.getuser()*10000
 
