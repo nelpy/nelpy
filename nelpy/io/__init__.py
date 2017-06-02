4766ce34925e3934442627585de593e89e81657a
eackermann <era@rice.edu>
2017-06-01 19:22:49 -0500

    ENH fixed readability

--- nelpy/io/__init__.py
+++ nelpy/io/__init__.py
@@ -9,7 +9,5 @@ from . import hc3
 from . import trodes
 from . import matlab
 from .cavaradossi import SDReader
-# from .hc3 import *
-# from .trodes import *
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
