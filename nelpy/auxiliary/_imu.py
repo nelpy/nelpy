7d81f30578e0917f4e30fbedc9d250e8e23dcd73
eackermann <etienne.ackermann@gmail.com>
2017-11-07 10:49:10 -0600

    MAINT added some explanatory text to IMU module

--- nelpy/auxiliary/_imu.py
+++ nelpy/auxiliary/_imu.py
@@ -54,3 +54,19 @@ class IMUSensorArray(_analogsignalarray.AnalogSignalArray):
     def only_imu_has_this_function(self):
         print("oh yeah!")
 
+
+"""
+Notes:
+------
+The IMUSensorArray class is meant to make using the IMU data easier.
+It is fundamentally still an AnalogSignalArray (hence the inheritance), but
+it should have all the specialized convenience functions built in, such as
+complementary filtering, orientation, unit-awareness, position estimation, and
+whatnot. The labels should also be set in a meaningful way.
+
+I think it is permissible to include a kind==['magnetometer', 'accelerometer', 'gyroscope']
+attribute, and to do specialized things depending on the type. However, for some
+filtering, we actually want to combine data from all three sensors, and so it
+might make more sense to have flexible definitions for a 3dof, 6dof 1dof, or 9dof
+sensor with understood types?
+"""
