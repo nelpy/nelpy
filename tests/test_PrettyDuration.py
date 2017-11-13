a5b8f5eb5ea4c7c0508a397c2c63da76ef957304
eackermann <etienne.ackermann@gmail.com>
2017-11-13 16:35:16 -0600

    improved PrettyDuration tests

--- tests/test_PrettyDuration.py
+++ tests/test_PrettyDuration.py
@@ -4,12 +4,12 @@ class TestPrettyDuration:
 
     def test_1(self):
         t = PrettyDuration(180)
-        assert t == '3:01 minutes'
+        assert str(t) == '3:00 minutes'
 
     def test_2(self):
-        t = PrettyDuration(179.9999999999999)
-        assert t == '3:00 minutes'
+        t = PrettyDuration(179.999999)
+        assert str(t) == '3:00 minutes'
 
     def test_3(self):
-        t = PrettyDuration(5.9999)
-        assert t == '6 seconds'
+        t = PrettyDuration(5.99999)
+        assert str(t) == '6 seconds'
