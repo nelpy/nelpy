eb9e0e51af6201af4f3ff0921b333d919bf8de57
eackermann <etienne.ackermann@gmail.com>
2017-12-08 10:39:04 -0600

    ENH added function definitions to PositionArray

--- nelpy/auxiliary/_position.py
+++ nelpy/auxiliary/_position.py
@@ -1,4 +1,5 @@
-# position data classes and methods
+"""Position class for 1D and 2D position AnalogSignalArrays"""
+
 import copy
 
 from ..core import _analogsignalarray, _epocharray #core._analogsignalarray import AnalogSignalArray
@@ -20,6 +21,7 @@ class PositionArray(_analogsignalarray.AnalogSignalArray):
             self._support = _epocharray.EpochArray(empty=True)
             return
 
+        # cast an AnalogSignalArray to a PositionArray:
         if isinstance(ydata, _analogsignalarray.AnalogSignalArray):
             self.__dict__ = copy.deepcopy(ydata.__dict__)
         else:
@@ -40,17 +42,78 @@ class PositionArray(_analogsignalarray.AnalogSignalArray):
         if self.isempty:
             return "<empty PositionArray" + address_str + ">"
         if self.n_epochs > 1:
-            epstr = " ({} segments)".format(self.n_epochs)
+            epstr = ": {} segments".format(self.n_epochs)
         else:
             epstr = ""
-        try:
-            if(self.n_signals > 0):
-                nstr = " %s signals%s" % (self.n_signals, epstr)
-        except IndexError:
-            nstr = " 1 signal%s" % epstr
         dstr = " for a total of {}".format(utils.PrettyDuration(self.support.duration))
-        return "<PositionArray%s:%s>%s" % (address_str, nstr, dstr)
+        if self.is_2d:
+            return "<2D PositionArray%s%s>%s" % (address_str, epstr, dstr)
+        return "<1D PositionArray%s%s>%s" % (address_str, epstr, dstr)
 
     def only_pos_has_this_function(self):
         print("oh yeah!")
 
+    @property
+    def is_2d(self):
+        try:
+            return self.n_signals == 2
+        except IndexError:
+            return False
+
+    @property
+    def is_1d(self):
+        try:
+            return self.n_signals == 1
+        except IndexError:
+            return False
+
+    @property
+    def x(self):
+        """return x values, as numpy array."""
+        return self.ydata[0,:]
+
+    @property
+    def y(self):
+        """return y values, as numpy array."""
+        if self.is_2d:
+            return self.ydata[1,:]
+        raise ValueError("PositionArray is not 2 dimensional, so y vlaues are undefined!")
+
+    @property
+    def path_length(self):
+        """Return the path length along the trajectory."""
+        raise NotImplementedError
+
+    def speed(self, sigma_pos=None, simga_spd=None):
+        """Return the speed, as an AnalogSignalArray."""
+        # Idea is to smooth the position with a good default, and then to
+        # compute the speed on the smoothed position. Optionally, then, the
+        # speed should be smoothed as well.
+        raise NotImplementedError
+
+    def direction(self):
+        """Return the instantaneous direction estimate as an AnalogSignalArray."""
+        # If 1D, then left/right or up/down or fwd/reverse or whatever might
+        # make sense, so this signal could be binarized.
+        # When 2D, then a continuous cartesian direction vector might make sense
+        # (that is, (x,y)-components), or a polar coordinate system might be
+        # better suited, with the direction signal being a continuous theta angle
+
+        raise NotImplementedError
+
+    def idealize(self, segments):
+        """Project the position onto idealized segments."""
+        # plan is to return a PositionArray constrained to the desired segments.
+
+        raise NotImplementedError
+
+    def linearize(self):
+        """Linearize the position estimates."""
+
+        # This is unclear how we should do it universally? Maybe have a few presets?
+
+        raise NotImplementedError
+
+    def bin(self, **kwargs):
+        """Bin position into grid."""
+        raise NotImplementedError
