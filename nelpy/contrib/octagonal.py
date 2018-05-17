97666b53ebfbc41eedf5c10225e14ba7eba2304a
eackermann <era@rice.edu>
2018-05-17 14:57:58 -0500

    ENH some updates to octagonal

--- nelpy/contrib/octagonal.py
+++ nelpy/contrib/octagonal.py
@@ -4,9 +4,64 @@ import copy
 import numpy as np
 
 from ..core import _analogsignalarray, _epocharray
+from .. auxiliary import _position
 from .. import utils
+from ..plotting.core import colorline
 
 #TODO: linsmooth(), wrap(), unwrap(), assign_segments(), lin_to_ideal(), ideal_to_lin(), 2d_to_ideal()
+
+def get_midpoint_radius(pos):
+    """Return the midpoint and radius of the hex maze as a tuple (x,y), radius.
+
+    Params
+    ======
+    pos: PositionArray
+        nelpy PositionArray containing the trajectory data.
+
+    Returns
+    =======
+    midpoint: (x0, y0)
+    radius: float
+    """
+    # make a local copy of the trajectory data
+    local_pos = copy.copy(pos)
+
+    # merge the underlyng support to make computations easier
+    local_pos._support = pos.support.merge(gap=10)
+
+    # apply smoothing to tame some outliers:
+    local_pos = local_pos.smooth(sigma=0.02)
+
+    midpoint = local_pos.min() + (local_pos.max() - local_pos.min())/2
+    radius = ((local_pos.max() - local_pos.min())/2).mean()
+
+    return midpoint, radius
+
+def excise_disk(pos, midpoint, radius, radius_pct=None):
+    """Excise all points within a disk from pos.
+
+    Params
+    ======
+    pos:
+    midpoint:
+    radius:
+    radius_pct: float
+        percent of radius within which to excise points. Default is 0.65 (65%).
+
+    Returns
+    =======
+    newpos:
+    """
+
+    if radius_pct is None:
+        radius_pct = 0.65
+
+    dist_to_midpoint = np.sqrt(((pos.ydata.T - midpoint)**2).sum(axis=1))
+    indisk_idx = np.argwhere(dist_to_midpoint > radius_pct*radius).squeeze()
+
+    local_pos = _position.PositionArray(pos.ydata[:,indisk_idx], timestamps=pos.time[indisk_idx])
+
+    return local_pos
 
 class OctagonalMazeTrajectory(_analogsignalarray.AnalogSignalArray):
 
@@ -44,6 +99,11 @@ class OctagonalMazeTrajectory(_analogsignalarray.AnalogSignalArray):
         # if self._vertices does not exist, then create it:
         if not '_vertices' in self.__dict__:
             self._vertices = None
+            self._segments = None
+            self._segment_lengths = None
+            self._segment_assignments = None
+            self._idealized = None
+            self._linearized = None
 
     def __repr__(self):
         address_str = " at " + str(hex(id(self)))
@@ -116,6 +176,17 @@ class OctagonalMazeTrajectory(_analogsignalarray.AnalogSignalArray):
 
         # TODO: do data integrity cheking / validation
         self._vertices = val
+        self._compute_segments()
+        self._compute_segment_lengths()
+        self._segment_assignments = None
+
+    @property
+    def segments(self):
+        return self._segments
+
+    @property
+    def segment_lengths(self):
+        return self._segment_lengths
 
     def speed(self, sigma_pos=None, simga_spd=None):
         """Return the speed, as an AnalogSignalArray."""
@@ -151,36 +222,25 @@ class OctagonalMazeTrajectory(_analogsignalarray.AnalogSignalArray):
         """Bin position into grid."""
         raise NotImplementedError
 
-    def unwrap(self, arr):
+    def _unwrap(self, arr):
 
         lin = copy.deepcopy(arr)
         for ii in range(1, len(lin)):
-            if lin[ii] - lin[ii-1] >= self.segments.length/2:
-                lin[ii:] = lin[ii:] - self.segments.length
-            elif lin[ii] - lin[ii-1] < -self.segments.length/2:
-                lin[ii:] = lin[ii:] + self.segments.length
+            if lin[ii] - lin[ii-1] >= self.track_length/2:
+                lin[ii:] = lin[ii:] - self.track_length
+            elif lin[ii] - lin[ii-1] < - self.track_length/2:
+                lin[ii:] = lin[ii:] + self.track_length
 
         return lin
 
     def _wrap(self, arr):
         return arr % self.track_length
 
-    @property
-    def track_length(self):
-        """The total idealized track length."""
-        raise NotImplementedError
+    def _compute_segment_assignments(self):
+        segment_assignments = self._get_closest_segments(self.ydata.T, self.segments)
+        self._segment_assignments = segment_assignments
 
-    @property
-    def unwrapped(self):
-        """Unwrapped linearized trajectory."""
-        raise NotImplementedError
-
-    def plot_track(self, show_points=True):
-        """Plot the idealized track."""
-        # todo: plot using colorline and show start, stop, and colorbar
-        raise NotImplementedError
-
-    def make_segments_from_vertices(self, vertices):
+    def _compute_segments(self):
         """make line segments from vertices
 
         a segment is of shape [[x0, y0], [x1, y2]]
@@ -193,7 +253,10 @@ class OctagonalMazeTrajectory(_analogsignalarray.AnalogSignalArray):
 
         """
 
-        v1, v2, v3, v4, v5, v6, v7, v8 = vertices
+        if self.vertices is None:
+            self._segments = None
+
+        v1, v2, v3, v4, v5, v6, v7, v8 = self.vertices
 
         a = [v1, v2]
         b = [v2, v3]
@@ -204,10 +267,73 @@ class OctagonalMazeTrajectory(_analogsignalarray.AnalogSignalArray):
         g = [v7, v8]
         h = [v8, v1]
 
-        vertices = np.array([v1, v2, v3, v4, v5, v6, v7, v8])
         segments = np.array([a,b,c,d,e,f,g,h])
 
-        return segments, vertices
+        self._segments = segments
+
+    @property
+    def segment_assignments(self):
+        if self._segment_assignments is None:
+            self._compute_segment_assignments()
+        return self._segment_assignments
+
+    def _compute_segment_lengths(self):
+        if self.vertices is None:
+            self._segment_lengths = None
+            self._segment_assignments = None
+
+        self._segment_lengths = np.sqrt(np.sum(np.diff(self._segments, axis=1)**2, axis=2)).squeeze()
+
+    @property
+    def track_length(self):
+        """The total idealized track length."""
+        if self.vertices is None:
+            return 0
+        return np.sum(self._segment_lengths)
+
+    @property
+    def unwrapped(self):
+        """Unwrapped linearized trajectory."""
+        raise NotImplementedError
+
+    def plot_track(self, show_points=True):
+        """Plot the idealized track.
+
+        segment_assignments = get_closest_segments(pos, segments)
+
+        xvals, yvals = pos.asarray().yvals
+        for ss in range(len(segments)):
+            subset = np.argwhere(segment_assignments==ss).squeeze()
+            plt.plot(xvals[subset], yvals[subset], '.', markersize=3)
+        plt.title('Initial segment assignment')
+        ax = plt.gca()
+        ax.set_aspect('equal')
+        plt.show()
+
+        """
+        import matplotlib.pyplot as plt
+        import matplotlib as mpl
+        # todo: plot using colorline and show start, stop, and colorbar
+        fig, ax = plt.subplots()
+
+        if show_points:
+            x, y = self.ydata
+            plt.plot(x, y, '.', color='0.3', markersize=1)
+        else:
+            xmin, ymin = self.min()
+            xmax, ymax = self.max()
+            ax.set_xlim(xmin,xmax)
+            ax.set_ylim(ymin,ymax)
+
+        x, y = self.vertices.T
+        colorline(np.insert(x,self.n_vertices,x[0]),np.insert(y, self.n_vertices, y[0]), cmap=plt.cm.Spectral_r, lw=5, cm_range=(0,1))
+        ax.set_aspect('equal')
+
+        cax = fig.add_axes([0.27, 0.95, 0.5, 0.05])
+        cb1 = mpl.colorbar.ColorbarBase(cax, cmap=plt.cm.Spectral_r,
+                                ticks=[0,1],
+                                orientation='horizontal')
+        cax.set_xticklabels(['start','stop'])
 
     def point_to_line_segment_dist(self, point, line_segment):
         """Calculate the distance between a point and a line segment.
@@ -261,7 +387,7 @@ class OctagonalMazeTrajectory(_analogsignalarray.AnalogSignalArray):
             # if not, then return the minimum distance to the segment endpoints
             return endpoint_dist
 
-    def get_closest_segments(self, pts, segments):
+    def _get_closest_segments(self, pts, segments):
         """for each point in the trajectory, determine the segment assignment (with smoothing and warning on ties)"""
         # TODO: warning not yet issued on ties
 
@@ -280,7 +406,7 @@ class OctagonalMazeTrajectory(_analogsignalarray.AnalogSignalArray):
         segment_assignments = np.argmin(dist_to_segments, axis=1, )
         return segment_assignments
 
-    def project_onto_segments(self, points, segments, segment_assignments):
+    def _project_onto_segments(self, points, segments, segment_assignments):
         """for each point, project onto assigned segment (this is idealized position, not linearized yet!)
 
         x1, x2, x3 = 5, 20, -25
@@ -341,13 +467,13 @@ class OctagonalMazeTrajectory(_analogsignalarray.AnalogSignalArray):
 
         return idealized
 
-    def linear_to_ideal(self, points, segments):
+    def _linear_to_ideal(self, points, segments):
         """transform points in the linearized space to points in the idealized (segment-bound) space."""
 
         if isinstance(points, _analogsignalarray.AnalogSignalArray):
-            pts = points.ydata.T
+            pts = points.ydata.T.squeeze()
         else:
-            pts = points
+            pts = points.squeeze()
 
         n_segments = len(segments)
         n_pts = np.size(pts)
@@ -355,7 +481,7 @@ class OctagonalMazeTrajectory(_analogsignalarray.AnalogSignalArray):
         segment_lengths = np.sqrt(np.sum(np.diff(segments, axis=1)**2, axis=2)).squeeze()
         cum_lengths = np.cumsum(segment_lengths)
         # identify appropriate segment
-        segment_assignments = np.searchsorted(cum_lengths, pts)
+        segment_assignments = np.searchsorted(cum_lengths, pts).squeeze()
         segment_assignments[segment_assignments >= n_segments] = n_segments - 1
 
         # get distance along each segment, for each point:
@@ -370,7 +496,18 @@ class OctagonalMazeTrajectory(_analogsignalarray.AnalogSignalArray):
 
         return new_coords
 
-    def ideal_to_linear(self, points, segments):
+    def linear_to_ideal(self, inplace=False):
+        new_coords = self._linear_to_ideal(self.linearized, self.segments)
+        out = copy.deepcopy(self)
+        out._ydata = new_coords.T
+        out._support = self.linearized.support
+
+        if inplace:
+            self._idealized = out
+
+        return out
+
+    def _ideal_to_linear(self, points, segments, segment_assignments=None):
         """transform points in the idealized (segment-based) space to points in the linearized space."""
 
         if isinstance(points, _analogsignalarray.AnalogSignalArray):
@@ -379,7 +516,8 @@ class OctagonalMazeTrajectory(_analogsignalarray.AnalogSignalArray):
             pts = points
 
         # identify appropriate segment
-        segment_assignments = self.get_closest_segments(pts, segments)
+        if segment_assignments is None:
+            segment_assignments = self.get_closest_segments(pts, segments)
 
         segment_lengths = np.sqrt(np.sum(np.diff(segments, axis=1)**2, axis=2)).squeeze()
         cum_lengths = np.cumsum(segment_lengths)
@@ -390,4 +528,82 @@ class OctagonalMazeTrajectory(_analogsignalarray.AnalogSignalArray):
         # determine distance from segment start point, and offset by cumulative segment lengths from start
         linearized = np.sqrt(np.sum((starts - pts)**2, axis=1)) + np.insert(cum_lengths,0,0)[segment_assignments]
 
+        if isinstance(points, _analogsignalarray.AnalogSignalArray):
+            out = copy.deepcopy(points)
+            out._ydata = np.atleast_2d(linearized)
+            linearized = out
+
         return linearized
+
+    @property
+    def idealized(self):
+        if self._idealized is None:
+            idealized = self._project_onto_segments(self, self.segments, self.segment_assignments)
+            self._idealized = idealized
+        return self._idealized
+
+    @property
+    def linearized(self):
+        if self._linearized is None:
+            linearized = self._ideal_to_linear(self.idealized, self.segments, self.segment_assignments)
+            linearized.smooth = linearized._smooth_unwrapped
+            self._linearized = linearized
+
+        return self._linearized
+
+    def _smooth_unwrapped(self, *, fs=None, sigma=None, bw=None, inplace=False):
+        """Smooths the regularly sampled AnalogSignalArray with a Gaussian kernel.
+
+        Smoothing is applied in time, and the same smoothing is applied to each
+        signal in the AnalogSignalArray.
+
+        Smoothing is applied within each epoch.
+
+        Parameters
+        ----------
+        fs : float, optional
+            Sampling rate (in Hz) of AnalogSignalArray. If not provided, it will
+            be obtained from asa.fs
+        sigma : float, optional
+            Standard deviation of Gaussian kernel, in seconds. Default is 0.05 (50 ms)
+        bw : float, optional
+            Bandwidth outside of which the filter value will be zero. Default is 4.0
+        inplace : bool
+            If True the data will be replaced with the smoothed data.
+            Default is False.
+
+        Returns
+        -------
+        out : AnalogSignalArray
+            An AnalogSignalArray with smoothed data is returned.
+        """
+        kwargs = {'inplace' : inplace,
+                'fs' : fs,
+                'sigma' : sigma,
+                'bw' : bw}
+        out = copy.deepcopy(self)
+        out._ydata = np.atleast_2d(out._unwrap(out.ydata.squeeze()))
+        out = utils.gaussian_filter(out, **kwargs)
+        out._ydata = np.atleast_2d(out._wrap(out.ydata.squeeze()))
+        if inplace:
+            self._ydata = out._ydata
+        out.__renew__()
+        self.__renew__()
+
+        # kwargs = {'inplace' : inplace,
+        #         'fs' : fs,
+        #         'sigma' : sigma,
+        #         'bw' : bw}
+        # ydata = copy.deepcopy(self.ydata)
+        # self._ydata = np.atleast_2d(self._unwrap(self.ydata.squeeze()))
+        # out = utils.gaussian_filter(self, **kwargs)
+        # out._ydata = np.atleast_2d(self._wrap(out.ydata.squeeze()))
+        # out.__renew__()
+
+        # if inplace:
+        #     self._ydata = out._ydata
+        # else:
+        #     self._ydata = ydata
+        # self.__renew__()
+
+        return out
