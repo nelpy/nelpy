39d63458621e59055d24e4977113c3a127fb6314
eackermann <era@rice.edu>
2018-11-11 15:43:25 -0600

    MAINT cleaned up preprocessing code

--- nelpy/preprocessing.py
+++ nelpy/preprocessing.py
@@ -430,76 +430,18 @@ class DataWindow(BaseEstimator):
             assert float(val) > 0, "``bin_width`` must be a non-negative number (float)!"
         self._bin_width = val
 
-    # def get_params(self):
-    #     """Dictionary of DataWindow parameters.
+    @property
+    def flatten(self):
+        return self._flatten
 
-    #     Can be used to instantiate a new DataWindow, for example:
-
-    #     >>> w_new = DataWindow(**w.get_params())
-    #     """
-    #     params = {'bins_before'  : self.bins_before,
-    #               'bins_after'   : self.bins_after,
-    #               'bins_current' : self.bins_current,
-    #               'bins_stride'  : self.bins_stride,
-    #               'bin_width'    : self.bin_width
-    #              }
-    #     return params
-
-    # def get_params(self, deep=True):
-    #     """Get parameters for this estimator.
-    #     Parameters
-    #     ----------
-    #     deep : boolean, optional
-    #         If True, will return the parameters for this estimator and
-    #         contained subobjects that are estimators.
-    #     Returns
-    #     -------
-    #     params : mapping of string to any
-    #         Parameter names mapped to their values.
-    #     """
-    #     out = dict()
-    #     for key in self._get_param_names():
-    #         value = getattr(self, key, None)
-    #         if deep and hasattr(value, 'get_params'):
-    #             deep_items = value.get_params().items()
-    #             out.update((key + '__' + k, val) for k, val in deep_items)
-    #         out[key] = value
-    #     return out
-
-    # def set_params(self, **params):
-    #     """Set the parameters of this estimator.
-    #     The method works on simple estimators as well as on nested objects
-    #     (such as pipelines). The latter have parameters of the form
-    #     ``<component>__<parameter>`` so that it's possible to update each
-    #     component of a nested object.
-    #     Returns
-    #     -------
-    #     self
-    #     """
-    #     if not params:
-    #         # Simple optimization to gain speed (inspect is slow)
-    #         return self
-    #     valid_params = self.get_params(deep=True)
-
-    #     nested_params = defaultdict(dict)  # grouped by prefix
-    #     for key, value in params.items():
-    #         key, delim, sub_key = key.partition('__')
-    #         if key not in valid_params:
-    #             raise ValueError('Invalid parameter %s for estimator %s. '
-    #                              'Check the list of available parameters '
-    #                              'with `estimator.get_params().keys()`.' %
-    #                              (key, self))
-
-    #         if delim:
-    #             nested_params[key][sub_key] = value
-    #         else:
-    #             setattr(self, key, value)
-    #             valid_params[key] = value
-
-    #     for key, sub_params in nested_params.items():
-    #         valid_params[key].set_params(**sub_params)
-
-    #     return self
+    @flatten.setter
+    def flatten(self, val):
+        try:
+            if val:
+                val = True
+        except Exception:
+            val = False
+        self._flatten = val
 
 
 class StreamingDataWindow():
