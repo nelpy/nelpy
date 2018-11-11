cefbad9da3c8d0ab078ded7d6176696766aa46ad
eackermann <era@rice.edu>
2018-11-11 15:37:44 -0600

    ENH DataWindow is now sklearn pipeline compatible

--- nelpy/preprocessing.py
+++ nelpy/preprocessing.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 
+from sklearn.base import BaseEstimator
+
 from sklearn.preprocessing import StandardScaler as SklearnStandardScaler
 from copy import copy as copycopy # to avoid name clash with local copy variable in StandardScaler
 
@@ -12,7 +14,7 @@ __all__ = ['DataWindow',
            'StreamingDataWindow',
            'StandardScaler']
 
-class DataWindow():
+class DataWindow(BaseEstimator):
     """
     DataWindow
     Data window description to describe stride and/or data aggregation.
@@ -45,13 +47,13 @@ class DataWindow():
             Total bin width = 5 seconds
     """
 
-    def __init__(self, bins_before=0, bins_after=0, bins_current=1, bins_stride=1, bin_width=None):
+    def __init__(self, bins_before=0, bins_after=0, bins_current=1, bins_stride=1, bin_width=None, flatten=False):
         self.bins_before = bins_before
         self.bins_after = bins_after
         self.bins_current = bins_current
         self.bins_stride = bins_stride
         self.bin_width = bin_width
-        pass
+        self._flatten = flatten
 
     def __str__(self):
         if self.bin_width is not None:
@@ -73,7 +75,23 @@ class DataWindow():
             .format(self.bins_before, self.bins_after, self.bins_current, self.bins_stride)
         return repr_string
 
-    def apply(self, X, T=None, lengths=None, flatten=False):
+    def fit(self, X, y, flatten=None):
+        """Dummy fit function to support sklean pipelines.
+        Parameters
+        ----------
+        X
+            Ignored
+        y
+            Ignored
+        flatten : bool, optional (default=False)
+            Whether or not to flatten the output data during transformation.
+        """
+        if flatten is not None:
+            self._flatten = flatten
+
+        return self
+
+    def transform(self, X, T=None, lengths=None, flatten=None):
         """
         Apply window specification to data in X.
 
@@ -112,6 +130,8 @@ class DataWindow():
         T : array-like of shape (n_samples,)
             Timestamps associated with data contained in Z.
         """
+        if flatten is None:
+            flatten = self._flatten
 
         X, T, lengths = self._tidy(X=X, T=T, lengths=lengths)
         z = []
@@ -127,7 +147,7 @@ class DataWindow():
 
         return Z, T
 
-    def _apply_contiguous(self, X, T=None, flatten=False):
+    def _apply_contiguous(self, X, T=None, flatten=None):
         """
         Apply window specification to data in X.
 
@@ -161,6 +181,8 @@ class DataWindow():
         T : array-like of shape (n_samples,)
             Timestamps associated with data contained in Z.
         """
+        if flatten is None:
+            flatten = self._flatten
 
         bins_before = self.bins_before
         bins_after = self.bins_after
@@ -408,20 +430,76 @@ class DataWindow():
             assert float(val) > 0, "``bin_width`` must be a non-negative number (float)!"
         self._bin_width = val
 
-    def get_params(self):
-        """Dictionary of DataWindow parameters.
+    # def get_params(self):
+    #     """Dictionary of DataWindow parameters.
 
-        Can be used to instantiate a new DataWindow, for example:
+    #     Can be used to instantiate a new DataWindow, for example:
 
-        >>> w_new = DataWindow(**w.get_params())
-        """
-        params = {'bins_before'  : self.bins_before,
-                  'bins_after'   : self.bins_after,
-                  'bins_current' : self.bins_current,
-                  'bins_stride'  : self.bins_stride,
-                  'bin_width'    : self.bin_width
-                 }
-        return params
+    #     >>> w_new = DataWindow(**w.get_params())
+    #     """
+    #     params = {'bins_before'  : self.bins_before,
+    #               'bins_after'   : self.bins_after,
+    #               'bins_current' : self.bins_current,
+    #               'bins_stride'  : self.bins_stride,
+    #               'bin_width'    : self.bin_width
+    #              }
+    #     return params
+
+    # def get_params(self, deep=True):
+    #     """Get parameters for this estimator.
+    #     Parameters
+    #     ----------
+    #     deep : boolean, optional
+    #         If True, will return the parameters for this estimator and
+    #         contained subobjects that are estimators.
+    #     Returns
+    #     -------
+    #     params : mapping of string to any
+    #         Parameter names mapped to their values.
+    #     """
+    #     out = dict()
+    #     for key in self._get_param_names():
+    #         value = getattr(self, key, None)
+    #         if deep and hasattr(value, 'get_params'):
+    #             deep_items = value.get_params().items()
+    #             out.update((key + '__' + k, val) for k, val in deep_items)
+    #         out[key] = value
+    #     return out
+
+    # def set_params(self, **params):
+    #     """Set the parameters of this estimator.
+    #     The method works on simple estimators as well as on nested objects
+    #     (such as pipelines). The latter have parameters of the form
+    #     ``<component>__<parameter>`` so that it's possible to update each
+    #     component of a nested object.
+    #     Returns
+    #     -------
+    #     self
+    #     """
+    #     if not params:
+    #         # Simple optimization to gain speed (inspect is slow)
+    #         return self
+    #     valid_params = self.get_params(deep=True)
+
+    #     nested_params = defaultdict(dict)  # grouped by prefix
+    #     for key, value in params.items():
+    #         key, delim, sub_key = key.partition('__')
+    #         if key not in valid_params:
+    #             raise ValueError('Invalid parameter %s for estimator %s. '
+    #                              'Check the list of available parameters '
+    #                              'with `estimator.get_params().keys()`.' %
+    #                              (key, self))
+
+    #         if delim:
+    #             nested_params[key][sub_key] = value
+    #         else:
+    #             setattr(self, key, value)
+    #             valid_params[key] = value
+
+    #     for key, sub_params in nested_params.items():
+    #         valid_params[key].set_params(**sub_params)
+
+    #     return self
 
 
 class StreamingDataWindow():
@@ -529,14 +607,14 @@ class StandardScaler(SklearnStandardScaler):
         if isinstance(X, (core.RegularlySampledAnalogSignalArray, core.BinnedEventArray)):
             if copy:
                 Xdata = copycopy(X.data.T)
+                X = X.copy()
             else:
                 Xdata = X.data.T
             Xdata = super().transform(Xdata, copy).T
 
-        if copy:
-            X = X.copy()
-
-        X._data = Xdata
+            X._data = Xdata
+        else:
+            X = super().transform(X, copy)
         return X
 
     def inverse_transform(self, X, copy=None):
@@ -559,13 +637,13 @@ class StandardScaler(SklearnStandardScaler):
         if isinstance(X, (core.RegularlySampledAnalogSignalArray, core.BinnedEventArray)):
             if copy:
                 Xdata = copycopy(X.data.T)
+                X = X.copy()
             else:
                 Xdata = X.data.T
             Xdata = super().inverse_transform(Xdata, copy).T
 
-        if copy:
-            X = X.copy()
-
-        X._data = Xdata
+            X._data = Xdata
+        else:
+            X = super().inverse_transform(X, copy)
 
         return X
