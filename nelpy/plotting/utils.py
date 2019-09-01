ad6c76a27fb204557eed7a22c0d154ee22be08b1
Caleb Kemere <ckemere@gmail.com>
2019-09-01 15:33:22 -0500

    Removed _hold in npl.imagesc. Replaced with a parameter 'clearaxes'. This was deprecated and then removed from Matplotlib.

--- nelpy/plotting/utils.py
+++ nelpy/plotting/utils.py
@@ -1010,8 +1010,8 @@ def imshow(axes, X, cmap=None, norm=None, aspect=None,
     Unlike matplotlib version, must explicitly specify axes
     """
 
-    if not axes._hold:
-        axes.cla()
+    # if not axes._hold:
+        # axes.cla()
     if norm is not None:
         assert(isinstance(norm, mcolors.Normalize))
     if aspect is None:
